85510c0a3cf80495c46c08a31023f4c4b29663c2
trombonist <yuto.fushimi@icloud.com>
2016-06-07 03:01:27 +0900

    first commit

---
+++ Assets/FPSController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSController : MonoBehaviour {
+
+	GameObject fps;
+
+	void Start () {
+		fps = GameObject.Find ("FPSCanvas");
+	}
+
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			fps.SetActive (!fps.activeSelf);
+		}
+	}
+}
